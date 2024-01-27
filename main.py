b275eabb87310c88c4705de8e8c4084197a5e3b7
secret1k <144740253+secret1k@users.noreply.github.com>
2024-01-27 15:15:39 +0300

    Movement, window change

--- main.py
+++ main.py
@@ -1,63 +1,74 @@
-
 import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QGraphicsRectItem, QDesktopWidget
 from PyQt5.QtCore import Qt, QTimer
 
-
-
 class Character(QGraphicsRectItem):
     def __init__(self):
-        super().__init__(0, 0, 1, 1)
-        self.setBrush(Qt.blue)
-        self.setFlag(QGraphicsRectItem.ItemIsFocusable)
+        super().__init__(0, 0, 50, 50)
+        # self.setBrush(Qt.blue)
+        # self.setFlag(QGraphicsRectItem.ItemIsFocusable)
         self.setFocus()
-        self.step = 4
-        self.move_directions = set()
-        self.animation_timer = QTimer()
-        self.animation_timer.timeout.connect(self.move)
-        self.animation_timer.start(10)
-
-
-    def keyPressEvent(self, event):
-        if event.text() in ['w','W','ц','Ц']:
-            self.move_directions.add("up")
-        elif event.text() in ['s','S','ы','Ы']:
-            self.move_directions.add("down")
-        elif event.text() in ['a','A','ф','Ф']:
-            self.move_directions.add("left")
-        elif event.text() in ['d','D','в','В']:
-            self.move_directions.add("right")
-
-    def keyReleaseEvent(self, event):
-        if event.text() in ['w','W','ц','Ц']:
-            self.move_directions.discard("up")
-        elif event.text() in ['s','S','ы','Ы']:
-            self.move_directions.discard("down")
-        elif event.text() in ['a','A','ф','Ф']:
-            self.move_directions.discard("left")
-        elif event.text() in ['d','D','в','В']:
-            self.move_directions.discard("right")
+        self.step = 5
+        self.X_SIZE_PLAYER = 50
+        self.Y_SIZE_PLAYER = 50
+        self.move_direction_U = 0
+        self.move_direction_D = 0
+        self.move_direction_L = 0
+        self.move_direction_R = 0
+        self.x = 0  # current position
+        self.y = 0  # current position
 
     def move(self):
-        current_pos = self.scenePos()
-        new_pos = current_pos
-        scene_rect = self.scene().sceneRect()
+        if self.x - self.step < 0:
+            self.x = 0
 
-        if "up" in self.move_directions:
-            new_pos.setY(current_pos.y() - self.step)
-        if "down" in self.move_directions:
-            new_pos.setY(current_pos.y() + self.step)
-        if "left" in self.move_directions:
-            new_pos.setX(current_pos.x() - self.step)
-        if "right" in self.move_directions:
-            new_pos.setX(current_pos.x() + self.step)
+        elif self.move_direction_L == 1:
+            self.x -= self.step
 
-        scene_rect = self.scene().sceneRect()
-        if not scene_rect.contains(new_pos):
-            new_pos.setX(max(scene_rect.left(), min(new_pos.x(), scene_rect.right() - self.rect().width())))
-            new_pos.setY(max(scene_rect.top(), min(new_pos.y(), scene_rect.bottom() - self.rect().height())))
+        if self.x + self.step > 800 - self.X_SIZE_PLAYER:
+            self.x = 800 - self.X_SIZE_PLAYER
+        elif self.move_direction_R == 1:
+            self.x += self.step
 
-        self.setPos(new_pos)
+        if self.y - self.step < 0:
+            self.y = 0
+        elif self.move_direction_U == 1:
+            self.y -= self.step
+
+        if self.y + self.step > 800 - self.Y_SIZE_PLAYER:
+            self.y = 800 - self.Y_SIZE_PLAYER
+        elif self.move_direction_D == 1:
+            self.y += self.step
+
+    def keyPressEvent(self, event):
+        if event.text() in ['Ц', 'ц', 'W', 'w']:
+            self.move_direction_U = 1
+
+        elif event.text() in ['Ф', 'ф', 'A', 'a']:
+            self.move_direction_L = 1
+
+        elif event.text() in ['Ы', 'ы', 'S', 's']:
+            self.move_direction_D = 1
+
+        elif event.text() in ['В', 'в', 'D', 'd']:
+            self.move_direction_R = 1
+
+    def keyReleaseEvent(self, event):
+        if event.text() in ['Ц', 'ц', 'W', 'w']:
+            self.move_direction_U = 0
+
+        elif event.text() in ['Ф', 'ф', 'A', 'a']:
+            self.move_direction_L = 0
+
+        elif event.text() in ['Ы', 'ы', 'S', 's']:
+            self.move_direction_D = 0
+
+        elif event.text() in ['В', 'в', 'D', 'd']:
+            self.move_direction_R = 0
+
 
 class Enemy(QGraphicsRectItem):
     def __init__(self, character):
@@ -82,29 +93,33 @@ class Enemy(QGraphicsRectItem):
 
 class Game(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.setWindowFlags(Qt.Window | Qt.FramelessWindowHint)
-        self.setWindowState(Qt.WindowFullScreen)
+        super(QMainWindow, self).__init__()
+
+        self.w = self.size().width()
+        self.h = self.size().height()
+
+        self.setGeometry(30, 30, 800, 800)
+        self.show()
         self.scene = QGraphicsScene(self)
         self.view = QGraphicsView(self.scene)
-        self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
-        screen_rect = QDesktopWidget().screenGeometry()
-        #self.setSceneRect(screen_rect)
-        #self.view.fitInView(self.sceneRect(), Qt.KeepAspectRatio)
-
-        self.setCentralWidget(self.view)
         self.character = Character()
-        self.character.setPos(self.width() / 2 - self.character.rect().width() / 2,
-                              self.height() / 2 - self.character.rect().height() / 2)
-        self.scene.addItem(self.character)
-        self.enemy = Enemy(self.character)
-        self.scene.addItem(self.enemy)
+
+        # self.character.setPos(self.width() / 2 - self.character.rect().width() / 2,
+        #                       self.height() / 2 - self.character.rect().height() / 2)
+        # self.scene.addItem(self.character)
+
+        # self.enemy = Enemy(self.character)
+        # self.scene.addItem(self.enemy)
 
         self.timer = QTimer()
-        self.timer.timeout.connect(self.enemy.move)
-        self.timer.start(10)
+        self.timer.timeout.connect(self.update_game)
+        self.timer.start(16)
+
+    def update_game(self):
+        self.character.move()
+        self.update()
+
 
     def keyPressEvent(self, event):
         self.character.keyPressEvent(event)
@@ -112,9 +127,22 @@ class Game(QMainWindow):
     def keyReleaseEvent(self, event):
         self.character.keyReleaseEvent(event)
 
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.fillRect(self.character.x, self.character.y, self.character.X_SIZE_PLAYER, self.character.Y_SIZE_PLAYER,
+                         QColor(10, 10, 150))
+
+    def resizeEvent(self, event):  # измените размер окна
+        print("Окно изменено")
+        QtWidgets.QMainWindow.resizeEvent(self, event)
+
+        self.width = self.size().width()
+        self.height = self.size().height()
+
+        print(self.width, self.height)  # актуальные размеры окна
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     game = Game()
-    game.showFullScreen()  # Отображение окна на весь экран
     sys.exit(app.exec_())
